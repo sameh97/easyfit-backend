fea95ab31000144104a945ee1a248b70a86b7ad4
sameh97 <sameh7462@gmail.com>
2021-03-14 15:26:44 +0200

    just added post method to the api

--- app.js
+++ app.js
@@ -1,19 +1,48 @@
-const express = require('express');
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const fs = require('fs');
 const app = express();
 
-app.use((req,res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-with, Content-type, Accept, Authoriztion");
-    if(req.method === "OPTIONS"){
-        res.header("Access-Control-Allow-Methods", "PUT,POST,PATCH,DELETE,GET");
-    }
-})
-
-app.get('/', (req,res) => {
-    res.send('<h2><center>sameh hassoun!!!!</center></h2>');
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("<h2><center>sameh hassoun!!!!</center></h2>");
+});
+
+app.post("/login", (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  console.log(`${email}, ${password}`);
+  res.send("hi");
+ 
+  if(validate(email,password)){
+    console.log(email);
+    //   res.redirect('/home-page')
+    //   fs.readFile('./home',null,(error,data) =>{
+    //       if(error){
+    //           res.writeHead(404);
+    //           res.write('file not found!');
+    //       }else{
+    //           res.write(data);
+    //       }
+    //       res.end();
+    //   }
+    //   );
+  }
+ 
+});
+
+function validate(email,password){
+    if(email === 'abc' && password === '123'){
+        return true;
+    }
+    return false;
+}
+
 module.exports = app;
-
-
--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const http = require('http');
 
 const app = require('./app.js');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 const server =  http.createServer(app);
 
 server.listen(PORT, () => {console.log(`Server started on port ${PORT}`);});
+
